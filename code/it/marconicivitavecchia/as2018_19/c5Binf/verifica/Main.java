04d102eaee95a62801853c9c4559972b589cb5fc
al <al.orchi@gm.com>
2018-11-12 14:41:42 +0100

    impty message aborts the commit.

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -12,16 +12,29 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(300,200);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb=new StringBuilder();
 		// TODO Creare la stringa HTML per il wireframe
-		
+		sb.append("<html>");
+		sb.append("<body>");
+		sb.append("<h1> Formula1 Passion </h1>");
+		sb.append("<br>");
+		sb.append("<div style=table>");
+		sb.append("<div> <tr> <th> Pilota </th> </div> <br>");
+		sb.append("<div> <th>Punteggio </th> </tr> </div>"); 	
+		sb.append("<div> <tr> <th>HAMILTON L. </th> </div> ");
+		sb.append("<div> <th> 358 </th> </tr> </div>"); 	
+		sb.append("<div> <tr> <th> VETTEL S. </th> </div>");
+		sb.append("<div> <th>294 </th> </tr> </div>");
+		sb.append("<div> <tr> <th> RAIKKONEN L. </th> </div>");
+		sb.append("<div>  <th>236 </th> </tr> </div>"); 	
+		sb.append("</html>");
 		// TODO Convertire lo StringBuilder in String
-		
+		String html=sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(html));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
